c9e29bf17abd6a29b378ac3a4d530a17df2b8bcf
Camilo <camilo.preciado336@gmail.com>
2026-05-16 20:34:10 -0500

    feat: agregar función calcular perímetro

--- funcion.py
+++ funcion.py
@@ -5,3 +5,12 @@ def calcular_area_rectangulo(base, altura):
 resultado = calcular_area_rectangulo(5, 4)
 
 print("El área es:", resultado)
+
+
+def calcular_perimetro_rectangulo(base, altura):
+    perimetro = 2 * (base + altura)
+    return perimetro
+
+resultado2 = calcular_perimetro_rectangulo(5, 4)
+
+print("El perímetro es:", resultado2)
